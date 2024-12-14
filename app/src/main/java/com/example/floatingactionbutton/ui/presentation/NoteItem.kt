dd29a87b83e0ef5e3c779a88637c43e40f4e8a6f
Muhammadarif98 <67822369+Muhammadarif98@users.noreply.github.com>
2024-12-14 21:16:57 +0300

    update, add alertDialog

--- app/src/main/java/com/example/floatingactionbutton/ui/presentation/NoteItem.kt
+++ app/src/main/java/com/example/floatingactionbutton/ui/presentation/NoteItem.kt
@@ -1,22 +1,69 @@
 package com.example.floatingactionbutton.ui.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.floatingactionbutton.R
 
 
 @Composable
 fun NoteItem(note: String, onDelete: () -> Unit) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Подтвердите удаление") },
+            text = {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painter = painterResource(R.drawable.delete), contentDescription = null)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text("Вы уверены, что хотите удалить заметку?")
+                }
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        showDialog = false
+                        onDelete()
+                    }
+                ) {
+                    Text("Удаление")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("Отмена")
+                }
+            }
+        )
+    }
+
     Row(
         modifier = Modifier
             .padding(vertical = 8.dp)
@@ -24,7 +71,7 @@ fun NoteItem(note: String, onDelete: () -> Unit) {
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = note)
-        IconButton(onClick = onDelete) {
+        IconButton(onClick = { showDialog = true }) {
             Icon(
                 imageVector = Icons.Default.Delete,
                 contentDescription = "Удалить заметку"
@@ -32,6 +79,7 @@ fun NoteItem(note: String, onDelete: () -> Unit) {
         }
     }
 }
+
 
 @Preview(showSystemUi = true)
 @Composable
